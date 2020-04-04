c9fdcf33cae6d66ecdaa877312c54af72096f646
Yashika Gupta <56039343+yashika-code-girl@users.noreply.github.com>
2020-04-05 00:45:13 +0530

    Add files via upload

---
+++ File_Handling_3/File_Handling_3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace File_Handling_3
+{
+    class Program
+    {
+        class FileRead
+        {
+            public void ReadData()
+            { 
+
+                //This will Open your file named yg which you create in File_Handling_2 project
+                FileStream fs = new FileStream("E:\\yg.txt", FileMode.Open, FileAccess.Read);
+                StreamReader sr = new StreamReader(fs);
+                Console.WriteLine("Program to show content of test file :");
+                sr.BaseStream.Seek(0, SeekOrigin.Begin);
+                string str = sr.ReadLine();
+                while (str != null)
+                {
+                    Console.WriteLine(str);
+                    str = sr.ReadLine();
+                }
+                Console.ReadLine();
+                sr.Close();
+                fs.Close();
+            }
+        }
+        static void Main(string[] args)
+        {
+            FileRead wr = new FileRead();
+            wr.ReadData();
+        }
+    }
+}
